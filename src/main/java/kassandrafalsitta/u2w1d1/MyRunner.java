f7f76b132d542ba2002249f727a17bfc05cd9c46
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-09-03 17:15:23 +0200

    fix

--- src/main/java/kassandrafalsitta/u2w1d1/MyRunner.java
+++ src/main/java/kassandrafalsitta/u2w1d1/MyRunner.java
@@ -1,10 +1,14 @@
 package kassandrafalsitta.u2w1d1;
 
+import kassandrafalsitta.u2w1d1.entities.FoodAndDrink;
+import kassandrafalsitta.u2w1d1.entities.Menu;
 import kassandrafalsitta.u2w1d1.entities.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Component
 public class MyRunner implements CommandLineRunner {
@@ -17,6 +21,8 @@ public class MyRunner implements CommandLineRunner {
     @Autowired
     @Qualifier("getOrder2")
     private Order order2;
+    @Autowired
+    Menu menu;
 
     @Override
     public void run(String... args) throws Exception {
@@ -26,7 +32,47 @@ public class MyRunner implements CommandLineRunner {
         System.out.println(order1.printOrderDetails());
         System.out.println(order2.printOrderDetails());
 
+        printPizzas();
+        printPizzasXL();
+        printToppings();
+        printDrinks();
+        System.out.println(menu);
 
+
+    }
+    public  void printPizzas() {
+        System.out.println("\n");
+        System.out.println("Pizzas                                                        Calories            Price");
+        menu.getPizze().forEach(pizza -> {
+            String toppings = pizza.getToppings().stream()
+                    .map(FoodAndDrink::getName)
+                    .collect(Collectors.joining(", "));
+            System.out.printf("%-10s %-45s %10d %18.2f\n", pizza.getName(), "(" + toppings + ")", pizza.getCalories(), pizza.getPrice());
+        });
+    }
+    public  void printPizzasXL() {
+        System.out.println("\n");
+        System.out.println("Pizzas XL                                                     Calories            Price");
+
+        menu.getPizzeXl().forEach(pizza -> {
+            String toppings = pizza.getToppings().stream()
+                    .map(FoodAndDrink::getName)
+                    .collect(Collectors.joining(", "));
+            System.out.printf("%-13s %-45s %8d %18.2f\n",pizza.getName(),"(" +toppings+")", pizza.getCalories(), pizza.getPrice());
+        });
+    }
+    public  void printToppings() {
+        System.out.println("\nToppings                                                      Calories            Price");
+        menu.getToppings().forEach(topping ->
+                System.out.printf("%-20s %46d %18.2f\n", topping.getName(), topping.getCalories(), topping.getPrice())
+        );
+    }
+    public  void printDrinks() {
+        System.out.println("\nDrinks                                                       Calories             Price");
+
+        menu.getDrinks().forEach(drink ->
+                System.out.printf("%-20s %46d %18.2f\n", drink.getName(), drink.getCalories(), drink.getPrice())
+        );
     }
 
 }
--- src/main/java/kassandrafalsitta/u2w1d1/U2w1d1Application.java
+++ src/main/java/kassandrafalsitta/u2w1d1/U2w1d1Application.java
@@ -11,53 +11,9 @@ import java.util.stream.Collectors;
 @SpringBootApplication
 public class U2w1d1Application {
 
-
-	static AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(U2w1d1Application.class);
-	static Menu menu = context.getBean(Menu.class);
 	public static void main(String[] args) {
 		SpringApplication.run(U2w1d1Application.class, args);
-		printPizzas();
-		printPizzasXL();
-		printToppings();
-		printDrinks();
 
-
-		context.close();
-	}
-
-	public static void printPizzas() {
-		System.out.println("\n");
-		System.out.println("Pizzas                                                        Calories            Price");
-		menu.getPizze().forEach(pizza -> {
-			String toppings = pizza.getToppings().stream()
-					.map(FoodAndDrink::getName)
-					.collect(Collectors.joining(", "));
-			System.out.printf("%-10s %-45s %10d %18.2f\n", pizza.getName(), "(" + toppings + ")", pizza.getCalories(), pizza.getPrice());
-		});
-	}
-	public static void printPizzasXL() {
-		System.out.println("\n");
-		System.out.println("Pizzas XL                                                     Calories            Price");
-
-		menu.getPizzeXl().forEach(pizza -> {
-			String toppings = pizza.getToppings().stream()
-					.map(FoodAndDrink::getName)
-					.collect(Collectors.joining(", "));
-			System.out.printf("%-13s %-45s %8d %18.2f\n",pizza.getName(),"(" +toppings+")", pizza.getCalories(), pizza.getPrice());
-		});
-	}
-	public static void printToppings() {
-		System.out.println("\nToppings                                                      Calories            Price");
-		menu.getToppings().forEach(topping ->
-				System.out.printf("%-20s %46d %18.2f\n", topping.getName(), topping.getCalories(), topping.getPrice())
-		);
-	}
-	public static void printDrinks() {
-		System.out.println("\nDrinks                                                       Calories             Price");
-
-		menu.getDrinks().forEach(drink ->
-				System.out.printf("%-20s %46d %18.2f\n", drink.getName(), drink.getCalories(), drink.getPrice())
-		);
 	}
 
 }
